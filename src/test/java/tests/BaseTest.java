95397a84cb3a8b6365a6423db9735fc981c68b12
Vishnuvardhan Pyatla <Vishnuvardhan.Pyatla@cognizant.com>
2026-01-28 18:10:03 +0530

    commited allure results integration

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,11 +1,11 @@
-
 package tests;
 
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestContext;
@@ -36,45 +36,71 @@ public class BaseTest {
     HomePage home;
     ResultPage res;
     TeamDetails td;
+
     @BeforeClass(alwaysRun = true)
     @Parameters({"browser","os"})
     public void setUp(ITestContext context,  @Optional("chrome") String browser,@Optional("windows") String os) throws IOException {
         FileReader file=new FileReader("src/test/resources/config.properties");
         p=new Properties();
         p.load(file);
+
+        boolean headless = Boolean.parseBoolean(p.getProperty("headless", "false"));
+
         if(p.getProperty("execution_env").equalsIgnoreCase("remote")){
-            DesiredCapabilities cap=new DesiredCapabilities();
-            if(os.equalsIgnoreCase("windows")){
-                cap.setPlatform(Platform.WIN11);
-            }
-            else if(os.equalsIgnoreCase("mac")){
-                cap.setPlatform(Platform.MAC);
-            }
-            else{
-                System.out.println("No matching OS");
-                return;
-            }
-            switch (browser.toLowerCase()) {
-                case "chrome":
-                    cap.setBrowserName("chrome");
-                    break;
-                case "edge":
-                    cap.setBrowserName("MicrosoftEdge");
-                    break;
-                default:
+            try {
+                if (browser.equalsIgnoreCase("chrome")) {
+                    ChromeOptions options = new ChromeOptions();
+                    if (headless) {
+                        options.addArguments("--headless=new");
+                        // fallback for older drivers: options.setHeadless(true);
+                    }
+                    if (os.equalsIgnoreCase("windows")) {
+                        options.setCapability("platformName", Platform.WIN11);
+                    } else if (os.equalsIgnoreCase("mac")){
+                        options.setCapability("platformName", Platform.MAC);
+                    } else {
+                        System.out.println("No matching OS");
+                        return;
+                    }
+                    driver = new RemoteWebDriver(new URL("http://10.232.48.253:4444/wd/hub"), options);
+                } else if (browser.equalsIgnoreCase("edge")) {
+                    EdgeOptions options = new EdgeOptions();
+                    if (headless) {
+                        options.addArguments("--headless=new");
+                    }
+                    if (os.equalsIgnoreCase("windows")) {
+                        options.setCapability("platformName", Platform.WIN11);
+                    } else if (os.equalsIgnoreCase("mac")){
+                        options.setCapability("platformName", Platform.MAC);
+                    } else {
+                        System.out.println("No matching OS");
+                        return;
+                    }
+                    driver = new RemoteWebDriver(new URL("http://10.232.48.253:4444/wd/hub"), options);
+                } else {
                     System.out.println("Invalid browser");
                     Reporter.log("Invalid browser parameter: " + browser, true);
                     return;
+                }
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to create remote driver", e);
             }
-            driver=new RemoteWebDriver(new URL("http://10.232.48.253:4444/wd/hub"),cap);
         }
         else if(p.getProperty("execution_env").equalsIgnoreCase("local")){
             switch (browser.toLowerCase()) {
                 case "chrome":
-                    driver = new ChromeDriver();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    if (headless) {
+                        chromeOptions.addArguments("--headless=new");
+                    }
+                    driver = new ChromeDriver(chromeOptions);
                     break;
                 case "edge":
-                    driver = new EdgeDriver();
+                    EdgeOptions edgeOptions = new EdgeOptions();
+                    if (headless) {
+                        edgeOptions.addArguments("--headless=new");
+                    }
+                    driver = new EdgeDriver(edgeOptions);
                     break;
                 default:
                     System.out.println("Invalid browser");
@@ -89,6 +115,7 @@ public class BaseTest {
         driver.get(baseUrl);
         context.setAttribute("driver", driver);
     }
+
     @AfterClass(alwaysRun = true)
     public void tearDown(ITestContext context) {
         log.info("Tearing down webdriver");
